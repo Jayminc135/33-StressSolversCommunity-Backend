d312e520751a5048c2e3a5af133e399f4f8aaa73
Vedantthakkar <vedantthakkar22@gmail.com>
2021-04-15 18:04:24 +0530

    Change in Login api, create and get event

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = mongoose.Schema({
     username:String,
     email: String,
+    isAdmin : Boolean
 },{ timestamps: { createdAt: 'created_at' } })
 
 
