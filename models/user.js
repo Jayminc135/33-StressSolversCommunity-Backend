2628b1f3bc8b78cdf8b000104b0b6bea6b6ac27e
Vedantthakkar <vedantthakkar22@gmail.com>
2021-05-06 22:38:13 +0530

    Bug fixes in Chat

--- models/user.js
+++ models/user.js
@@ -1,15 +1,9 @@
 const mongoose = require('mongoose')
-const InboxObject = new mongoose.Schema({
-    from: String,
-    message: String
-})
+
 const userSchema = mongoose.Schema({
     username:String,
     email: String,
     isAdmin : Boolean,
-    friends : [new mongoose.Schema({
-        inbox: [InboxObject]
-    })]
 },{ timestamps: { createdAt: 'created_at' } })
 
 
